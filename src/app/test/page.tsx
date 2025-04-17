3268b7d3f6b3bcd918442d92b9cb61ec5be1667e
PJ Premacio <pj.premacio@raybiztech.com>
2025-04-17 17:20:40 +0800

    added init files

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Test Page</h1>
+            <p>This is a boilerplate React component.</p>
+        </div>
+    );
+};
+
+export default TestPage;
